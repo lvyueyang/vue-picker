2c71427a01dda6197eeb86bf8cf248ff22c92ed1
lvyueyang <975794403@qq.com>
2020-04-21 12:54:07 +0800

    改名

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist/',
-        filename: 'v-picker-m.js'
+        filename: 'vue-picker.js'
     },
     module: {
         rules: [
